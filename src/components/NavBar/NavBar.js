40f1d6be4ad4b6df270b21c3fe47cba5964d2004
Joaquin Sangiorgi <joaquinsangiorgi2002@gmail.com>
2022-11-07 20:37:35 -0300

    Estilos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import "./NavBar.scss"
+import { Icon } from '@iconify/react';
 
 const NavBar = () => {
   return (
     <header className='header-container'>
     
-      <div className='header-links-container'>
+      <ul className='header-links-container'>
 
-          <a href='#'>Sobre mi</a>
-          <a href='#'>Proyectos</a>
-          <a href='#'>Contacto</a>
           
-      </div>
+          <li href='#'><Icon className='header-icons' icon="ant-design:user-outlined" />Sobre mi</li>
+          <li href='#'><Icon className='header-icons' icon="grommet-icons:projects" />Proyectos</li>
+          <li href='#'><Icon className='header-icons' icon="fluent:contact-card-16-regular" />Contacto</li>
+          
+      </ul>
       
     </header>
   )
